e955c1a93fce1fdb2943b073cbf1bc882f376a5c
Tuan Le <tuanle@Tuans-MacBook-Pro.local>
2026-04-10 23:56:59 +0700

    feat: [backend] dockerize backend and run with docker-compose

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://notes_user:notes_password@localhost:5432/notes_db"
+DATABASE_URL = "postgresql://notes_user:notes_password@db:5432/notes_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
